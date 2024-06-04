c29ef2eddfb779a771578db101281967bd071ace
AdamUzDel <adamssemakula@gmail.com>
2024-06-04 18:35:03 +0300

    Initial commit - plain

---
+++ src/main/java/com/bytebasetech/everything_java_beginner/EVERYTHING_JAVA_BEGINNER.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bytebasetech.everything_java_beginner;
+
+/**
+ *
+ * @author BYTEBASE-ACE
+ */
+public class EVERYTHING_JAVA_BEGINNER {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
